73e244b883888a3b8252ec0b797d11b3e56a29e3
AnkitSamal16 <126022706+AnkitSamal16@users.noreply.github.com>
2024-03-31 00:02:56 +0530

    Complete Weather App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import WeatherApp from './Components/WeatherApp/WeatherApp'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <WeatherApp />
+    </div>
+  )
+}
+
+export default App
